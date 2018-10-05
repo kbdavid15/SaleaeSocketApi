2cefccbdf861c568332b6db12cea71a26e586b76
Kyle David <kyle.david@trw.com>
2018-10-05 17:39:12 -0400

    updated to support variable number of channels

--- SaleaeUartLogger/Program.cs
+++ SaleaeUartLogger/Program.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using Saleae.SocketApi;
 
 namespace SaleaeUartLogger
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             RunProgram();
         }
@@ -48,12 +47,12 @@ namespace SaleaeUartLogger
 
             //Get a list of connected devices. (when no devices are connected, this should return 4 simulation devices)
             var devices = client.GetConnectedDevices();
-            var active_device = devices.Single(x => x.IsActive);
+            var activeDevice = devices.Single(x => x.IsActive);
 
             Console.WriteLine("currently available devices:");
             devices.ToList().ForEach(x => Console.WriteLine(x.Name));
 
-            Console.WriteLine("currently active device: " + active_device.Name);
+            Console.WriteLine("currently active device: " + activeDevice.Name);
             Console.WriteLine("");
 
             //list all analyzers currently added to the session.
@@ -67,55 +66,84 @@ namespace SaleaeUartLogger
             }
 
             //change the active channels, but only if the active device supports that.
-            if (active_device.DeviceType == DeviceType.Logic8 || active_device.DeviceType == DeviceType.LogicPro8 ||
-                active_device.DeviceType == DeviceType.LogicPro16)
+            if (activeDevice.DeviceType != DeviceType.Logic8 && activeDevice.DeviceType != DeviceType.LogicPro8 &&
+                activeDevice.DeviceType != DeviceType.LogicPro16)
             {
-                Console.WriteLine("changing active channels");
-                client.SetActiveChannels(new[] { 0 });
-                Console.WriteLine("");
-                //showing active channels
-                var active_digital_channels = new List<int>();
-                var active_analog_channels = new List<int>();
-                client.GetActiveChannels(active_digital_channels, active_analog_channels);
-                Console.WriteLine("Active Digital Channels: " + string.Join(", ", active_digital_channels.ToArray()));
-                Console.WriteLine("Active Analog Channels: " + string.Join(", ", active_analog_channels.ToArray()));
-                //display the currently selected sample rate and performance option.
-                var current_sample_rate = client.GetSampleRate();
-                Console.WriteLine("The previously selected sample rate was: " + current_sample_rate.DigitalSampleRate +
-                                  " SPS (digital), " + current_sample_rate.AnalogSampleRate + " SPS (analog)");
-                if (current_sample_rate.AnalogSampleRate > 0 && current_sample_rate.DigitalSampleRate > 0)
+                Console.WriteLine("No supported device connected. Press any key to exit");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.Write("Enter number of channels to log: ");
+            var numChannels = Console.ReadLine();
+
+            if (int.TryParse(numChannels, out var iNumChannels) && iNumChannels <= 8)
+            {
+                var channelsToActivate = new int[iNumChannels];
+                for (var i = 0; i < channelsToActivate.Length; i++)
                 {
-                    var current_performance_option = client.GetPerformanceOption();
-                    Console.WriteLine("Currently selected performance option: " + current_performance_option);
+                    channelsToActivate[i] = i;
                 }
 
-                //change the sample rate!
-                var possible_sample_rates = client.GetAvailableSampleRates();
-                if (possible_sample_rates.Any(x => x.DigitalSampleRate == sampleRate))
-                {
-                    Console.WriteLine($"Changing digital sample rate to {sampleRate}");
-                    client.SetSampleRate(possible_sample_rates.First(x => x.DigitalSampleRate == sampleRate));
-                    Console.WriteLine("");
-                }
-                else
-                {
-                    Console.WriteLine("Selected sample rate not available, exiting");
-                    return;
-                }
-
-                //set trigger. There are 4 digital channels. all need to be specified.
-                Console.WriteLine("setting trigger");
-                client.SetTrigger(new[] {Trigger.FallingEdge});
+                Console.WriteLine($"Activating channels {string.Join(" ", channelsToActivate)}");
+                client.SetActiveChannels(channelsToActivate);
                 Console.WriteLine("");
             }
             else
             {
-                Console.WriteLine(
-                    "to see more cool features demoed by this example, please switch to a Logic 8, Logic Pro 8, or Logic Pro 16. Physical or simulation");
+                Console.WriteLine("Invalid entry, only using channel 0");
+                client.SetActiveChannels(new[] {0});
+                Console.WriteLine("");
             }
 
-            if (active_device.DeviceType == DeviceType.LogicPro8 || active_device.DeviceType == DeviceType.LogicPro16 ||
-                active_device.DeviceType == DeviceType.Logic16)
+
+            //showing active channels
+            var activeDigitalChannels = new List<int>();
+            var activeAnalogChannels = new List<int>();
+            client.GetActiveChannels(activeDigitalChannels, activeAnalogChannels);
+            Console.WriteLine("Active Digital Channels: " + string.Join(", ", activeDigitalChannels.ToArray()));
+            Console.WriteLine("Active Analog Channels: " + string.Join(", ", activeAnalogChannels.ToArray()));
+            //display the currently selected sample rate and performance option.
+            var currentSampleRate = client.GetSampleRate();
+            Console.WriteLine("The previously selected sample rate was: " + currentSampleRate.DigitalSampleRate +
+                              " SPS (digital), " + currentSampleRate.AnalogSampleRate + " SPS (analog)");
+            if (currentSampleRate.AnalogSampleRate > 0 && currentSampleRate.DigitalSampleRate > 0)
+            {
+                var currentPerformanceOption = client.GetPerformanceOption();
+                Console.WriteLine("Currently selected performance option: " + currentPerformanceOption);
+            }
+
+            //change the sample rate!
+            var possibleSampleRates = client.GetAvailableSampleRates();
+            if (possibleSampleRates.Any(x => x.DigitalSampleRate == sampleRate))
+            {
+                Console.WriteLine($"Changing digital sample rate to {sampleRate}");
+                client.SetSampleRate(possibleSampleRates.First(x => x.DigitalSampleRate == sampleRate));
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("Selected sample rate not available, exiting");
+                return;
+            }
+
+            //set trigger. There are 4 digital channels. all need to be specified.
+            Console.WriteLine("Setting trigger to CH0 falling edge");
+
+            var triggers = new Trigger[activeDigitalChannels.Count];
+
+            for (var i = 0; i < triggers.Length; i++)
+            {
+                triggers[i] = Trigger.None;
+            }
+
+            triggers[0] = Trigger.FallingEdge;
+
+            client.SetTrigger(triggers);
+            Console.WriteLine("");
+
+            if (activeDevice.DeviceType == DeviceType.LogicPro8 || activeDevice.DeviceType == DeviceType.LogicPro16 ||
+                activeDevice.DeviceType == DeviceType.Logic16)
             {
                 var voltageOption = client.GetDigitalVoltageOptions().SingleOrDefault(x => x.IsSelected);
                 if (voltageOption != null)
@@ -123,7 +151,7 @@ namespace SaleaeUartLogger
             }
             // get the recording time
             Console.WriteLine("");
-            Console.WriteLine("Enter the capture time in seconds. Equal to the power cycle time");
+            Console.Write("Enter the capture time in seconds. Equal to the power cycle time: ");
             var captureTimeString = Console.ReadLine();
             if (!double.TryParse(captureTimeString, out var captureTimeSec))
             {
@@ -136,12 +164,32 @@ namespace SaleaeUartLogger
             client.SetCaptureSeconds(captureTimeSec);
             Console.WriteLine("");
 
-            var defaultFileName = "Output.csv";
-            Console.WriteLine($"Enter sample number or file name identifier or Enter for default: [{defaultFileName}]");
-            var fileNameId = Console.ReadLine();
-            if (fileNameId == string.Empty) fileNameId = defaultFileName;
 
-            Console.WriteLine("Press Enter to Begin Capture");
+            var fileNameIds = new string[activeDigitalChannels.Count];
+            var fileDirectories = new string[activeDigitalChannels.Count];
+            for (var i = 0; i < activeDigitalChannels.Count; i++)
+            {
+                var activeDigitalChannel = activeDigitalChannels[i];
+                var defaultFileName = $"CH{activeDigitalChannel}_output";
+                Console.Write(
+                    $"Enter file name identifier for channel {activeDigitalChannel} or Enter for default [{defaultFileName}]: ");
+                var fileNameId = Console.ReadLine();
+                if (fileNameId == string.Empty) fileNameId = defaultFileName;
+
+                fileNameIds[i] = fileNameId;
+
+                var directoryName = $"{Directory.GetCurrentDirectory()}\\{fileNameId}_{DateTime.Now:yyyy-MM-dd}";
+
+                if (!Directory.Exists(directoryName))
+                {
+                    Directory.CreateDirectory(directoryName);
+                }
+
+                fileDirectories[i] = directoryName;
+            }
+
+
+            Console.Write("Press Enter to Begin Capture");
             Console.ReadLine();
             Console.WriteLine("Press ESC to stop");
 
@@ -155,14 +203,18 @@ namespace SaleaeUartLogger
                     client.Capture();
 
                     // once capture is complete, save the data to file
-                    var filePath =
-                        $"{Directory.GetCurrentDirectory()}\\{fileNameId}_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.csv";
-                    client.ExportAnalyzers(analyzers.First().Index, filePath, false);
+                    for (var i = 0; i < analyzers.Count; i++)
+                    {
+                        if (fileNameIds.Length <= i) continue;
+                        var filePath =
+                            $"{fileDirectories[i]}\\{fileNameIds[i]}_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.csv";
+                        client.ExportAnalyzers(analyzers[i].Index, filePath, false);
 
-                    Console.WriteLine($"Exported data capture #{captureCounter} to {filePath}");
+                        Console.WriteLine($"Exported {analyzers[i].AnalyzerType} analyzer #{analyzers[i].Index} data capture #{captureCounter} to {filePath}");
+                    }
+
+
                     captureCounter++;
-                    Console.WriteLine("Sleeping for 1 second");
-                    Thread.Sleep(1000);
                 }       
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
